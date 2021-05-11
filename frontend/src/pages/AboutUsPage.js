e1c0a75058ebb68f7d8dbd69af34eba564a4e7c9
Emre <memreyildiz99@gmail.com>
2021-05-11 21:50:14 +0300

    About Us v2

--- frontend/src/pages/AboutUsPage.js
+++ frontend/src/pages/AboutUsPage.js
@@ -1,63 +1,10 @@
 import {Container, Row, Col, Image, Card, Button, Spinner} from "react-bootstrap";
-import React, {useContext, useEffect, useState} from "react";
-// import axios from "axios";
-// import {useParams} from "react-router-dom";
+
 import '../style/my_cart.css';
-import {FiShoppingCart} from "react-icons/fi"
-import {useHistory, useParams} from "react-router-dom";
-import axios from "axios";
-import {NotificationContext} from "../services/NotificationContext";
-import {AuthContext} from "../services/AuthContext";
-
-export default function MyCartPage(){
 
 
-    const {setShow, setContent, setIntent} = useContext(NotificationContext);
-    const {getCurrentUser} = useContext(AuthContext);
-    const history = useHistory();
-    const [user, setUser] = useState(null);
-    const [courseList, setCourseList] = useState(null);
+export default function AboutUsPage(){
 
-    useEffect(async () => {
-        let response = await getCurrentUser;
-        setUser(response?.data);
-        axios.get('/api/user/cart-courses/' + response?.data.id)
-            .then(response => {
-                setCourseList(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, []);
-
-    const handleByRemove = async (index) => {
-        axios.delete('/api/user/remove-cart/' + courseList[index].id.toString(10))
-            .then(response => {
-                let tmp = [...courseList]
-                tmp.splice(index,1)
-                setCourseList(tmp)
-                index--
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
-
-
-
-
-    if(courseList == null || courseList?.length === 0 ){
-        return (<Container className="mt-5">
-            <h2>You don't have any Courses</h2>
-            <h3>
-                You can buy Courses from
-                <span className="actions">
-        <a className="btn" href="/" >Homepage</a>
-    </span>
-            </h3>
-
-        </Container>);
-    }
 
     return (
         <div id="my_cart_div">
